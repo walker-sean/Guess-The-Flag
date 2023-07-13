ae8f32eb496495a3931cea57548968ff9422361b
walker-sean <walker.se@northeastern.edu>
2023-07-13 19:13:44 -0400

    Epic button time

--- Guess The Flag/ContentView.swift
+++ Guess The Flag/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+        Button {
+            print("Editing")
+        } label: {
+            Label("Edit", systemImage: "pencil")
+        }
     }
 }
 
