1a86398ae6957088ddce9d8863577e6dc6949c72
walker-sean <walker.se@northeastern.edu>
2023-07-30 13:55:21 -0400

    Basis of game created w/ flags

--- Guess The Flag/ContentView.swift
+++ Guess The Flag/ContentView.swift
@@ -8,17 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        Button {
-            print("Editing")
-        } label: {
-            Label("Edit", systemImage: "pencil")
-        }
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of").foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0..<3) {number in
+                    Button {
+                        
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
