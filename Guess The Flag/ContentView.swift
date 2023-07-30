16d99248b0458313dfb62392f30b355ba7adec23
walker-sean <walker.se@northeastern.edu>
2023-07-30 14:06:56 -0400

    Working game with score

--- Guess The Flag/ContentView.swift
+++ Guess The Flag/ContentView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+   @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+        .shuffled()
+   @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
     
     var body: some View {
         ZStack {
@@ -24,14 +28,34 @@ struct ContentView: View {
                 
                 ForEach(0..<3) {number in
                     Button {
-                        
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
                     }
                 }
             }
+        }.alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(score)")
         }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
     
     struct ContentView_Previews: PreviewProvider {
